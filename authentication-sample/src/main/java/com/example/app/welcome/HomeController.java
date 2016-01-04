7fa18498b77cddd715b1941759aeaf4c5175c0d7
ikeya <tm.ikeya@gmail.com>
2016-01-05 00:58:17 +0900

    #1: support detection of outbound request with requestUri

--- authentication-sample/src/main/java/com/example/app/welcome/HomeController.java
+++ authentication-sample/src/main/java/com/example/app/welcome/HomeController.java
@@ -10,10 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 
